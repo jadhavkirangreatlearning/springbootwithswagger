d565b0f93a3f1cd7723bc2481476b19bb5d01107
jadhavkirangreatlearning <jadhavkiran.greatlearning@gmail.com>
2022-12-13 11:21:25 +0530

    spring boot with swagger ui

---
+++ custmgntswagger/src/main/java/com/csi/repo/CustomerRepo.java
@@ -0,0 +1,11 @@
+package com.csi.repo;
+
+import com.csi.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepo extends JpaRepository<Customer, Integer> {
+    //
+
+    
+
+}
